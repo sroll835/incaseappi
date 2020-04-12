51dbe4881cef76fbbe251bb1bc14f398fb8626b7
sroll835 <sebastiancaceres98@hotmail.com>
2020-04-12 17:58:15 -0500

    users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsuariosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(), UsuariosModule],
 })
 export class AppModule {}
--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { Usuario } from './usuario.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Usuario])],
   providers: [UsuariosService],
-  controllers: [UsuariosController]
+  controllers: [UsuariosController],
 })
 export class UsuariosModule {}
