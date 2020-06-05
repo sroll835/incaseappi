821e5f3cd934a399557f422c8086edf580e3a82f
sroll835 <sebastiancaceres98@hotmail.com>
2020-06-04 23:30:02 -0500

    atributo imagen

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -6,7 +6,7 @@ import { Usuario } from './usuario.entity';
 @Injectable()
 export class UsuariosService {
   constructor(
-    @InjectRepository(Usuario) private usersRepository: Repository<Usuario>
+    @InjectRepository(Usuario) private usersRepository: Repository<Usuario>,
   ) {}
 
   async createUser(user: Usuario) {
@@ -19,19 +19,19 @@ export class UsuariosService {
 
   async getUser(id_usuario: string): Promise<Usuario[]> {
     return await this.usersRepository.find({
-      select: ['nombre', 'apellido', 'correo', 'clave'],
+      select: ['nombre', 'apellido', 'correo', 'imagen', 'clave'],
       where: [{ id_usuario: id_usuario }],
     });
   }
 
   async getCursosByUserId(id_usuario: string): Promise<Usuario[]> {
-    return await this.usersRepository.createQueryBuilder("usuario")
-    .leftJoinAndSelect("usuario.cursos", "curso")
-    .where("usuario.id_usuario = :id", { id: id_usuario})
-    .getMany();
+    return await this.usersRepository
+      .createQueryBuilder('usuario')
+      .leftJoinAndSelect('usuario.cursos', 'curso')
+      .where('usuario.id_usuario = :id', { id: id_usuario })
+      .getMany();
   }
-  
-  
+
   async updateUser(user: Usuario) {
     this.usersRepository.save(user);
   }
